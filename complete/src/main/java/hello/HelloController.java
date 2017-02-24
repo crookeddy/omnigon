9825721a10faf2570dcf47a885f0b692349b5bb6
Oleg Gnatovskiy <ognatovskiy@cj.com>
2017-02-24 15:10:21 -0800

    Trying to fix git

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -1,14 +1,28 @@
 package hello;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.omnigon.string.domain.StringList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class HelloController {
     
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    @RequestMapping(value="/", method = RequestMethod.POST)
+    public ResponseEntity<List<String>> getStringWithLongestWord(@RequestBody StringList sl) {
+    	List<String> strings = new ArrayList<>();
+        strings.add("a");
+        strings.add("b");
+        strings.add(sl.getStrings().get(1));
+        return new ResponseEntity<List<String>>(strings, HttpStatus.OK);
     }
     
 }
